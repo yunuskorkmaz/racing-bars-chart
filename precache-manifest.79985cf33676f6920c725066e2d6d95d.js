9e61044e9ef70fa2608da7ad5044c35295a59182
Yunus Korkmaz <yunuskorkmaz95@gmail.com>
2020-05-13 14:34:38 +0300

    Updates

---
+++ precache-manifest.79985cf33676f6920c725066e2d6d95d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23a74368ab9f39d5c4c10a72b3ddf42",
+    "url": "/racing-bars-chart/index.html"
+  },
+  {
+    "revision": "e14f999b9fd4c1fa22bc",
+    "url": "/racing-bars-chart/static/css/main.2e2ba254.chunk.css"
+  },
+  {
+    "revision": "920d304740a218ebbcf7",
+    "url": "/racing-bars-chart/static/js/2.d6a89c99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/racing-bars-chart/static/js/2.d6a89c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14f999b9fd4c1fa22bc",
+    "url": "/racing-bars-chart/static/js/main.db4e5979.chunk.js"
+  },
+  {
+    "revision": "4392f097daeddff82d61",
+    "url": "/racing-bars-chart/static/js/runtime-main.f4f45586.js"
+  }
+]);
